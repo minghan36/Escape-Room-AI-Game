99cee8db15295275dcd641f5373243c5d69d181e
minghan36 <mhua948@aucklanduni.ac.nz>
2023-09-11 00:48:01 +1200

    Feat: connected all three rooms
    All three rooms can be visited. Opacity changed when hovering over room changing buttons.

--- src/main/java/nz/ac/auckland/se206/controllers/BathroomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/BathroomController.java
@@ -1,5 +1,26 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.se206.App;
+
 public class BathroomController {
     
+    @FXML private Rectangle toLockedRoom;
+
+    @FXML
+    public void enterLockedRoom(MouseEvent event) {
+        App.setUi("lockedroom");
+    }
+
+    @FXML
+    public void highlight(){
+        toLockedRoom.setOpacity(0.7);
+    }
+
+    @FXML 
+    public void removeHighlight(){
+        toLockedRoom.setOpacity(0.4);
+    }
 }
--- src/main/java/nz/ac/auckland/se206/controllers/LockedRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LockedRoomController.java
@@ -1,20 +1,16 @@
 package nz.ac.auckland.se206.controllers;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+//import javafx.scene.control.Alert;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.App;
-import nz.ac.auckland.se206.GameState;
 
 /** Controller class for the room view. */
 public class LockedRoomController {
 
-  @FXML private Rectangle door;
-  @FXML private Rectangle window;
-  @FXML private Rectangle vase;
+  @FXML private Rectangle toBathroom;
+  @FXML private Rectangle toComputerRoom;
 
   /** Initializes the room view, it is called when the room loads. */
   public void initialize() {
@@ -47,7 +43,7 @@ public class LockedRoomController {
    * @param title the title of the dialog box
    * @param headerText the header text of the dialog box
    * @param message the message content of the dialog box
-   */
+   *
   private void showDialog(String title, String headerText, String message) {
     Alert alert = new Alert(Alert.AlertType.INFORMATION);
     alert.setTitle(title);
@@ -55,52 +51,34 @@ public class LockedRoomController {
     alert.setContentText(message);
     alert.showAndWait();
   }
-
-  /**
-   * Handles the click event on the door.
-   *
-   * @param event the mouse event
-   * @throws IOException if there is an error loading the chat view
-   */
+*/
   @FXML
-  public void clickDoor(MouseEvent event) throws IOException {
-    System.out.println("door clicked");
-
-    if (!GameState.isRiddleResolved) {
-      showDialog("Info", "Riddle", "You need to resolve the riddle!");
-      App.setRoot("chat");
-      return;
-    }
-
-    if (!GameState.isKeyFound) {
-      showDialog(
-          "Info", "Find the key!", "You resolved the riddle, now you know where the key is.");
-    } else {
-      showDialog("Info", "You Won!", "Good Job!");
-    }
+  public void enterBathroom(){
+    App.setUi("bathroom");
   }
 
-  /**
-   * Handles the click event on the vase.
-   *
-   * @param event the mouse event
-   */
   @FXML
-  public void clickVase(MouseEvent event) {
-    System.out.println("vase clicked");
-    if (GameState.isRiddleResolved && !GameState.isKeyFound) {
-      showDialog("Info", "Key Found", "You found a key under the vase!");
-      GameState.isKeyFound = true;
-    }
+  public void enterComputerRoom(){
+    App.setUi("computerroom");
   }
 
-  /**
-   * Handles the click event on the window.
-   *
-   * @param event the mouse event
-   */
   @FXML
-  public void clickWindow(MouseEvent event) {
-    System.out.println("window clicked");
+  public void highlightBathroom(){
+    toBathroom.setOpacity(0.7);
+  }
+
+  @FXML 
+  public void removeHighlightBathroom(){
+    toBathroom.setOpacity(0.4);
+  }
+
+  @FXML
+  public void highlightComputerRoom(){
+    toComputerRoom.setOpacity(0.7);
+  }
+
+  @FXML 
+  public void removeHighlightComputerRoom(){
+    toComputerRoom.setOpacity(0.4);
   }
 }
