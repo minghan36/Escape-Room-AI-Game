71bdcbafb8bc1b8a9cb27b26a413628644b740aa
sdes755 <sdes755@aucklanduni.ac.nz>
2023-09-10 20:41:25 +1200

    Feat: Created functionality for selecting the time and difficulty level of the game, and indicating to the users when their choice is locked in. Also added an objectives area, which will be implemented later on.

--- src/main/java/nz/ac/auckland/se206/controllers/IntroController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/IntroController.java
@@ -3,19 +3,33 @@ package nz.ac.auckland.se206.controllers;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.GameState;
 
 public class IntroController {
-  @FXML private Rectangle level;
+
   @FXML private Rectangle time;
   @FXML private Rectangle Start;
   @FXML private Label title;
+  @FXML private Button easy;
+  @FXML private Button medium;
+  @FXML private Button difficult;
   private boolean levelIsPicked;
   private boolean timeIsPicked;
+  @FXML private Label chooseEasy;
+  @FXML private Label chooseMedium;
+  @FXML private Label chooseDifficult;
+  @FXML private Label minTwo;
+  @FXML private Label minFour;
+  @FXML private Label minSix;
+  @FXML private Button twoMin;
+  @FXML private Button fourMin;
+  @FXML private Button sixMin;
 
   public void initialize() {
     // create a translate transition for the label
@@ -47,7 +61,7 @@ public class IntroController {
   @FXML
   public void startGame(MouseEvent event) {
     if (levelIsPicked && timeIsPicked) {
-      App.setUi("room");
+      App.setUi("lockedroom");
     } else {
       showDialog("Info", "select", "You need to select a time and level to start the game");
     }
@@ -59,5 +73,71 @@ public class IntroController {
     alert.setHeaderText(headerText);
     alert.setContentText(message);
     alert.showAndWait();
+  }
+
+  @FXML
+  private void easyPicked(MouseEvent event) {
+    levelIsPicked = true;
+    chooseEasy.setText("CHOSEN");
+    chooseDifficult.setText("");
+    chooseMedium.setText("");
+    GameState.isEasyPicked = true;
+    GameState.isMediumPicked = false;
+    GameState.isDifficultPicked = false;
+  }
+
+  @FXML
+  private void mediumPicked(MouseEvent event) {
+    levelIsPicked = true;
+    GameState.isMediumPicked = true;
+    GameState.isEasyPicked = false;
+    GameState.isDifficultPicked = false;
+    chooseMedium.setText("CHOSEN");
+    chooseDifficult.setText("");
+    chooseEasy.setText("");
+  }
+
+  @FXML
+  private void difficultPicked(MouseEvent event) {
+    levelIsPicked = true;
+    GameState.isDifficultPicked = true;
+    GameState.isEasyPicked = false;
+    GameState.isMediumPicked = false;
+    chooseDifficult.setText("CHOSEN");
+    chooseEasy.setText("");
+    chooseMedium.setText("");
+  }
+
+  @FXML
+  private void twoPicked(MouseEvent event) {
+    timeIsPicked = true;
+    GameState.isTwoMinutesPicked = true;
+    GameState.isFourMinutesPicked = false;
+    GameState.isSixMinutesPicked = false;
+    minTwo.setText("CHOSEN");
+    minFour.setText("");
+    minSix.setText("");
+  }
+
+  @FXML
+  private void fourPicked(MouseEvent event) {
+    timeIsPicked = true;
+    GameState.isFourMinutesPicked = true;
+    GameState.isTwoMinutesPicked = false;
+    GameState.isSixMinutesPicked = false;
+    minFour.setText("CHOSEN");
+    minTwo.setText("");
+    minSix.setText("");
+  }
+
+  @FXML
+  private void sixPicked(MouseEvent event) {
+    timeIsPicked = true;
+    GameState.isSixMinutesPicked = true;
+    GameState.isTwoMinutesPicked = false;
+    GameState.isFourMinutesPicked = false;
+    minSix.setText("CHOSEN");
+    minTwo.setText("");
+    minFour.setText("");
   }
 }
