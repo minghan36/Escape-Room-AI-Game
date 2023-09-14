c6439c5fa1a927131c929c1ff2b72711c6c3189c
sdes755 <sdes755@aucklanduni.ac.nz>
2023-09-15 00:18:36 +1200

    feat:Implemented the timer in the controller class and fixed an error in the computerroom controller class where the method was named wrong.

--- src/main/java/nz/ac/auckland/se206/controllers/DecryptController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/DecryptController.java
@@ -1,3 +1,49 @@
 package nz.ac.auckland.se206.controllers;
 
-public class DecryptController {}
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+import nz.ac.auckland.se206.GameState;
+
+public class DecryptController {
+
+  @FXML private Label Timer;
+
+  public void initialize() {
+    Timer.setText(GameState.getTimeLeft());
+    Thread timeThread =
+        new Thread(
+            () -> {
+              startTimer();
+            });
+    timeThread.start();
+  }
+
+  public void startTimer() {
+    Timeline timeline =
+        new Timeline(
+            new KeyFrame(
+                Duration.seconds(1),
+                new EventHandler<ActionEvent>() {
+                  @Override
+                  public void handle(ActionEvent event) {
+                    // Counts down the timer.
+                    Platform.runLater(
+                        new Runnable() {
+                          @Override
+                          public void run() {
+                            Timer.setText(GameState.getTimeLeft());
+                          }
+                        });
+                  }
+                }));
+
+    timeline.setCycleCount((GameState.minutes * 60) + GameState.seconds - 1);
+    timeline.play();
+  }
+}
