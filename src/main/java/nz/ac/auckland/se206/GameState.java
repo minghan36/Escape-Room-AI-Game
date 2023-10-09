a604996a141fff22a4141034a97acb647d00d06a
sdes755 <sdes755@aucklanduni.ac.nz>
2023-10-09 13:39:23 +1300

    Style: Imlpemented commenting and formatting for the files stated on the Codestyle report.

--- src/main/java/nz/ac/auckland/se206/GameState.java
+++ src/main/java/nz/ac/auckland/se206/GameState.java
@@ -16,16 +16,18 @@ public class GameState {
 
   /** Indicates whether the riddle has been resolved. */
   public static boolean isRiddleResolved = false;
+
   // Creating/Intialising variables for the global aspect fo the game
   public static String currentRoom = "lockedroom";
   public static String chatContents = "";
   public static String latestHint = "";
   // Running the first instance of GPT
-  public static ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest().setN(1).setTemperature(0.1)
-      .setTopP(0.5).setMaxTokens(100);
+  public static ChatCompletionRequest chatCompletionRequest =
+      new ChatCompletionRequest().setN(1).setTemperature(0.1).setTopP(0.5).setMaxTokens(100);
 
   /** Indicates whether the key has been found. */
   public static boolean isKeyFound = false;
+
   // Booleans for game progression
   public static boolean isElectricalTapeFound = false;
 
@@ -59,17 +61,17 @@ public class GameState {
   // choose a random number between zero and two
   public static int randomNum = (int) (Math.random() * 3);
   // create string array having first second third
-  public static String[] randomLights = { "first", "second", "third" };
+  public static String[] randomLights = {"first", "second", "third"};
   public static String randomLight = randomLights[randomNum];
 
   public static String[] objectives = {
-      "Riddle", "Decrypt", "Light Puzzle", "Picture Puz", "RGB Puzzle"
+    "Riddle", "Decrypt", "Light Puzzle", "Picture Puz", "RGB Puzzle"
   };
   // Intialising variables for the objectives
   public static String currentObj = objectives[0];
   public static String objMessage = "";
   public static String hintMessage = "";
-  public static String[] riddleAnswers = { "jupiter", "galaxy", "star" };
+  public static String[] riddleAnswers = {"jupiter", "galaxy", "star"};
 
   public static String riddleAnswer = riddleAnswers[randomNum];
 
@@ -89,40 +91,42 @@ public class GameState {
   }
 
   // Intialising variables for the random letters
-  public static String[] randomLetters = { "⍀", "☌", "⏚", "⊬" };
-  public static String password = randomLetters[(int) (Math.random() * 4)]
-      + randomLetters[(int) (Math.random() * 4)]
-      + randomLetters[(int) (Math.random() * 4)]
-      + randomLetters[(int) (Math.random() * 4)];
+  public static String[] randomLetters = {"⍀", "☌", "⏚", "⊬"};
+  public static String password =
+      randomLetters[(int) (Math.random() * 4)]
+          + randomLetters[(int) (Math.random() * 4)]
+          + randomLetters[(int) (Math.random() * 4)]
+          + randomLetters[(int) (Math.random() * 4)];
 
   // Intialising the timer for the game
   public static void startTimer() {
-    timeline = new Timeline(
-        new KeyFrame(
-            Duration.seconds(1),
-            new EventHandler<ActionEvent>() {
-              @Override
-              public void handle(ActionEvent event) {
-                // Counts down the timer.
-                if (GameState.seconds == 0) {
-                  GameState.minutes--;
-                  GameState.seconds = 59;
-                } else if (GameState.seconds > 0) {
-                  GameState.seconds--;
-                }
-                if (GameState.minutes == 0 && GameState.seconds == 0) {
-                  timeline.stop();
-                  // set room to End
-                  GameState.currentRoom = "End";
-                  try {
-                    App.setRoot("End");
-                  } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+    timeline =
+        new Timeline(
+            new KeyFrame(
+                Duration.seconds(1),
+                new EventHandler<ActionEvent>() {
+                  @Override
+                  public void handle(ActionEvent event) {
+                    // Counts down the timer.
+                    if (GameState.seconds == 0) {
+                      GameState.minutes--;
+                      GameState.seconds = 59;
+                    } else if (GameState.seconds > 0) {
+                      GameState.seconds--;
+                    }
+                    if (GameState.minutes == 0 && GameState.seconds == 0) {
+                      timeline.stop();
+                      // set room to End
+                      GameState.currentRoom = "End";
+                      try {
+                        App.setRoot("End");
+                      } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                      }
+                    }
                   }
-                }
-              }
-            }));
+                }));
 
     timeline.setCycleCount((GameState.minutes * 60) + GameState.seconds);
     timeline.play();
@@ -141,9 +145,11 @@ public class GameState {
 
   // Intialising the chat system and prompt system of the GPT API
   public static void sendPrompt(String prompt) {
+    // Adding the message to the chat
     chatCompletionRequest.addMessage(new ChatMessage("user", prompt));
     ChatCompletionResult chatCompletionResult;
     try {
+      // Getting the response from the GPT API
       chatCompletionResult = chatCompletionRequest.execute();
       chatCompletionResult.getChoices().iterator().next();
     } catch (ApiProxyException e) {
@@ -177,11 +183,13 @@ public class GameState {
       // For the light puzzle state
     } else if (GameState.currentObj == "Light Puzzle") {
       if (!isLightPuzzleStarted) {
-        objMessage = "Find the light which was mentioned in the message and fix it to get the next"
-            + " clue.";
+        objMessage =
+            "Find the light which was mentioned in the message and fix it to get the next"
+                + " clue.";
         return objMessage;
       } else if (isLightPuzzleStarted && !isElectricalTapeFound) {
-        objMessage = "Find the Electrical Tape needed to patch the wires. Check carefully in each room!";
+        objMessage =
+            "Find the Electrical Tape needed to patch the wires. Check carefully in each room!";
         return objMessage;
       } else if (isElectricalTapeFound) {
         objMessage = "Good job you have found the electrical tape, now fix the wires.";
@@ -195,22 +203,25 @@ public class GameState {
         objMessage = "You need to find the picture of the Globe to access the next puzzle!";
         return objMessage;
       } else if (isGlobeFound) {
-        objMessage = "You have found the picture of the Globe. Navigate to the desired room and find the"
-            + " object, using the clue you have been given to access the puzzle.";
+        objMessage =
+            "You have found the picture of the Globe. Navigate to the desired room and find the"
+                + " object, using the clue you have been given to access the puzzle.";
         return objMessage;
       } else if (!isPuzzleSolved) {
         objMessage = "You need to solve the picture puzzle to receive your next clue!";
         return objMessage;
       } else {
-        objMessage = "You have solved the picture puzzle. Collect the RGB Clue and travel to the door for"
-            + " your final puzzle";
+        objMessage =
+            "You have solved the picture puzzle. Collect the RGB Clue and travel to the door for"
+                + " your final puzzle";
         return objMessage;
       }
       // For the RGB puzzle state
     } else if (GameState.currentObj == "RGB Puzzle") {
       if (!isRgbSolved) {
-        objMessage = "You need to solve the RGB puzzle to escape the room. Use the given clue and the alien"
-            + " alphabet on the computer to unlock the passcode.";
+        objMessage =
+            "You need to solve the RGB puzzle to escape the room. Use the given clue and the alien"
+                + " alphabet on the computer to unlock the passcode.";
         return objMessage;
       } else {
         objMessage = "You have solved the RGB puzzle. You have escaped the room!";
--- src/main/java/nz/ac/auckland/se206/controllers/PuzController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/PuzController.java
@@ -32,40 +32,73 @@ import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
 
 public class PuzController {
-  @FXML private Rectangle p1;
-  @FXML private Rectangle p2;
-  @FXML private Rectangle p3;
-  @FXML private Rectangle p4;
-  @FXML private Rectangle p5;
-  @FXML private Rectangle p6;
-  @FXML private Rectangle p7;
-  @FXML private Rectangle p8;
-  @FXML private Rectangle p9;
-  @FXML private ImageView pic1;
-  @FXML private ImageView pic2;
-  @FXML private ImageView pic3;
-  @FXML private ImageView pic4;
-  @FXML private ImageView pic5;
-  @FXML private ImageView pic6;
-  @FXML private ImageView pic7;
-  @FXML private ImageView pic8;
-  @FXML private ImageView pic9;
-  @FXML private Label status;
-  @FXML private Label timer;
-  @FXML private Button check;
-  @FXML private Button goBackBtn;
-  @FXML private Canvas gameMaster;
-  @FXML private Rectangle quizMaster;
+  // Intialising variables required for the room and the puzzle
+  @FXML
+  private Rectangle p1;
+  @FXML
+  private Rectangle p2;
+  @FXML
+  private Rectangle p3;
+  @FXML
+  private Rectangle p4;
+  @FXML
+  private Rectangle p5;
+  @FXML
+  private Rectangle p6;
+  @FXML
+  private Rectangle p7;
+  @FXML
+  private Rectangle p8;
+  @FXML
+  private Rectangle p9;
+  @FXML
+  private ImageView pic1;
+  @FXML
+  private ImageView pic2;
+  @FXML
+  private ImageView pic3;
+  @FXML
+  private ImageView pic4;
+  @FXML
+  private ImageView pic5;
+  @FXML
+  private ImageView pic6;
+  @FXML
+  private ImageView pic7;
+  @FXML
+  private ImageView pic8;
+  @FXML
+  private ImageView pic9;
+  @FXML
+  private Label status;
+  @FXML
+  private Label timer;
+  @FXML
+  private Button check;
+  @FXML
+  private Button goBackBtn;
+  @FXML
+  private Canvas gameMaster;
+  @FXML
+  private Rectangle quizMaster;
   private Image[] alienImages;
   private int currentImageIndex = 0;
-  @FXML private TextArea objText;
-  @FXML private TextArea hintsText;
-  @FXML private ImageView tape;
-  @FXML private ImageView sdCard;
-  @FXML private ImageView globe;
-  @FXML private Button rgbClue;
-  @FXML private Button rgbClue1;
-  @FXML private Label collect;
+  @FXML
+  private TextArea objText;
+  @FXML
+  private TextArea hintsText;
+  @FXML
+  private ImageView tape;
+  @FXML
+  private ImageView sdCard;
+  @FXML
+  private ImageView globe;
+  @FXML
+  private Button rgbClue;
+  @FXML
+  private Button rgbClue1;
+  @FXML
+  private Label collect;
 
   private List<Rectangle> rectangles;
   private List<ImageView> imageViews;
@@ -78,6 +111,7 @@ public class PuzController {
   private Map<Rectangle, int[]> positionMap = new HashMap<>();
 
   public void initialize() {
+    // Displaying the items which are collected/not collected
     hintsText.setText(GameState.getHint());
     if (GameState.isPuzzleSolved && !GameState.isRgbClueFound) {
       rgbClue.setVisible(true);
@@ -102,25 +136,24 @@ public class PuzController {
     sdCard.setVisible(GameState.isSdCardFound);
     globe.setVisible(GameState.isGlobeFound);
     timer.setText(GameState.getTimeLeft());
-    Thread timeThread =
-        new Thread(
-            () -> {
-              startTimer();
-            });
+    // Starting the timer thread for the room
+    Thread timeThread = new Thread(
+        () -> {
+          startTimer();
+        });
     timeThread.start();
-    alienImages =
-        new Image[] {
-          new Image("images/move1.png"),
-          new Image("images/move2.png"),
-          new Image("images/move3.png"),
-          new Image("images/move4.png")
-        };
+    alienImages = new Image[] {
+        new Image("images/move1.png"),
+        new Image("images/move2.png"),
+        new Image("images/move3.png"),
+        new Image("images/move4.png")
+    };
 
     // Start the animation
     startAnimation();
+    // Starting the animations for the gamemaster
 
-    TranslateTransition translateTransition =
-        new TranslateTransition(Duration.seconds(2), gameMaster);
+    TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(2), gameMaster);
 
     // set the Y-axis translation value
     translateTransition.setByY(-10);
@@ -134,15 +167,15 @@ public class PuzController {
     // Start the animation
     translateTransition.play();
     // Define position (row, column) for each rectangle
-    positionMap.put(p1, new int[] {0, 0});
-    positionMap.put(p2, new int[] {0, 1});
-    positionMap.put(p3, new int[] {0, 2});
-    positionMap.put(p4, new int[] {1, 0});
-    positionMap.put(p5, new int[] {1, 1});
-    positionMap.put(p6, new int[] {1, 2});
-    positionMap.put(p7, new int[] {2, 0});
-    positionMap.put(p8, new int[] {2, 1});
-    positionMap.put(p9, new int[] {2, 2});
+    positionMap.put(p1, new int[] { 0, 0 });
+    positionMap.put(p2, new int[] { 0, 1 });
+    positionMap.put(p3, new int[] { 0, 2 });
+    positionMap.put(p4, new int[] { 1, 0 });
+    positionMap.put(p5, new int[] { 1, 1 });
+    positionMap.put(p6, new int[] { 1, 2 });
+    positionMap.put(p7, new int[] { 2, 0 });
+    positionMap.put(p8, new int[] { 2, 1 });
+    positionMap.put(p9, new int[] { 2, 2 });
 
     rectangles = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9);
     imageViews = Arrays.asList(pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9);
@@ -159,29 +192,29 @@ public class PuzController {
     }
   }
 
+  // Creating and intialising the gamemaster animation
   private void startAnimation() {
     GraphicsContext gc = gameMaster.getGraphicsContext2D();
-    AnimationTimer timer =
-        new AnimationTimer() {
-          private long lastTime = 0;
-          private final long frameDurationMillis = 100; // 1000 milliseconds = 1 second
+    AnimationTimer timer = new AnimationTimer() {
+      private long lastTime = 0;
+      private final long frameDurationMillis = 100; // 1000 milliseconds = 1 second
 
-          @Override
-          public void handle(long currentTime) {
-            if (currentTime - lastTime >= frameDurationMillis * 1_000_000) {
-              if (currentImageIndex < alienImages.length) {
-                gc.clearRect(0, 0, gameMaster.getWidth(), gameMaster.getHeight());
-                gc.drawImage(alienImages[currentImageIndex], 0, 0);
-                currentImageIndex++;
-                // Check if we have displayed all images; if so, reset the index to 0
-                if (currentImageIndex >= alienImages.length) {
-                  currentImageIndex = 0;
-                }
-                lastTime = currentTime;
-              }
+      @Override
+      public void handle(long currentTime) {
+        if (currentTime - lastTime >= frameDurationMillis * 1_000_000) {
+          if (currentImageIndex < alienImages.length) {
+            gc.clearRect(0, 0, gameMaster.getWidth(), gameMaster.getHeight());
+            gc.drawImage(alienImages[currentImageIndex], 0, 0);
+            currentImageIndex++;
+            // Check if we have displayed all images; if so, reset the index to 0
+            if (currentImageIndex >= alienImages.length) {
+              currentImageIndex = 0;
             }
+            lastTime = currentTime;
           }
-        };
+        }
+      }
+    };
     timer.start();
   }
 
@@ -191,29 +224,32 @@ public class PuzController {
     App.setUi("chat");
   }
 
+  // Method to start the timer for the scene
   private void startTimer() {
-    Timeline timeline =
-        new Timeline(
-            new KeyFrame(
-                Duration.seconds(1),
-                new EventHandler<ActionEvent>() {
-                  @Override
-                  public void handle(ActionEvent event) {
-                    // Counts down the timer.
-                    Platform.runLater(
-                        new Runnable() {
-                          @Override
-                          public void run() {
-                            timer.setText(GameState.getTimeLeft());
-                          }
-                        });
-                  }
-                }));
+    Timeline timeline = new Timeline(
+        new KeyFrame(
+            // Setting the time countdown to one second
+            Duration.seconds(1),
+            new EventHandler<ActionEvent>() {
+              @Override
+              public void handle(ActionEvent event) {
+                // Counts down the timer.
+                Platform.runLater(
+                    new Runnable() {
+                      @Override
+                      public void run() {
+                        // Displaying the time left on the label
+                        timer.setText(GameState.getTimeLeft());
+                      }
+                    });
+              }
+            }));
 
     timeline.setCycleCount((GameState.minutes * 60) + GameState.seconds - 1);
     timeline.play();
   }
 
+  // Method to split the original image into 9 pieces for the picture puzzle
   private List<Image> splitImage(Image image) {
     List<Image> pieces = new ArrayList<>();
     PixelReader reader = image.getPixelReader();
@@ -224,14 +260,14 @@ public class PuzController {
 
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
-        WritableImage piece =
-            new WritableImage(reader, j * pieceWidth, i * pieceHeight, pieceWidth, pieceHeight);
+        WritableImage piece = new WritableImage(reader, j * pieceWidth, i * pieceHeight, pieceWidth, pieceHeight);
         pieces.add(piece);
       }
     }
     return pieces;
   }
 
+  // Method to check if the two selected pieces are adjacent
   private boolean isAdjacent(Rectangle first, Rectangle second) {
     int[] pos1 = positionMap.get(first);
     int[] pos2 = positionMap.get(second);
@@ -243,6 +279,7 @@ public class PuzController {
     return horizontal || vertical;
   }
 
+  // Method to check if the puzzle is in the correct order
   private boolean isCorrectOrder() {
     for (int i = 0; i < 9; i++) {
       if (!imageViews.get(i).getImage().equals(correctOrder.get(i))) {
@@ -252,6 +289,7 @@ public class PuzController {
     return true;
   }
 
+  // Method to swap the two selected pieces
   @FXML
   private void swap(MouseEvent event) {
     Rectangle clicked = (Rectangle) event.getSource();
@@ -269,23 +307,26 @@ public class PuzController {
     }
   }
 
+  // Method to check if the puzzle is solved
   @FXML
   private void onCheckPuzzle(ActionEvent event) {
+    // If the puzzle is solved, display the correct message
     if (isCorrectOrder()) {
       status.setText("Correct");
       GameState.isPuzzleSolved = true;
       objText.setText(
           "Good Job! You have solved the picture puzzle. Collect the RGB Clue and travel to the"
               + " door for your final puzzle");
-      Thread thread =
-          new Thread(
-              () -> {
-                GameState.sendPrompt(
-                    "The player has solved the jigsaw puzzle and has received the passcode to the"
-                        + " locked door. The passcode is in an alien language and can be translated"
-                        + " using the alien alphabet on the computer. Each letter of the passcode"
-                        + " represents a coloured button.");
-              });
+      // Thread to send the prompt to the GPT to over look progress
+      Thread thread = new Thread(
+          () -> {
+            GameState.sendPrompt(
+                "The player has solved the jigsaw puzzle and has received the passcode to the"
+                    + " locked door. The passcode is in an alien language and can be translated"
+                    + " using the alien alphabet on the computer. Each letter of the passcode"
+                    + " represents a coloured button.");
+          });
+      // Starting the thread and setting game items
       thread.start();
       rgbClue.setVisible(true);
       rgbClue.setText(GameState.password);
@@ -296,6 +337,7 @@ public class PuzController {
     }
   }
 
+  // Method to swap the two selected pieces
   private void swapImages(Rectangle first, Rectangle second) {
     ImageView firstImage = map.get(first);
     ImageView secondImage = map.get(second);
@@ -312,13 +354,15 @@ public class PuzController {
     firstSelected = null; // Resetting firstSelected for next selection
   }
 
+  // Method to animate the swap of the two selected pieces
   private void animateSwap(ImageView first, ImageView second) {
+    // Create two translate transitions, one for each image
     TranslateTransition tt1 = new TranslateTransition();
     TranslateTransition tt2 = new TranslateTransition();
-
+    // Set the dpositions of the images
     double dx = second.getX() - first.getX();
     double dy = second.getY() - first.getY();
-
+    // Swapping the x and y coordinates of the images
     tt1.setByX(dx);
     tt1.setByY(dy);
 
@@ -327,6 +371,7 @@ public class PuzController {
 
     tt1.setNode(first);
     tt2.setNode(second);
+    // Playing the animation for the two images
 
     tt1.play();
     tt2.play();
@@ -340,6 +385,7 @@ public class PuzController {
         });
   }
 
+  // Method to go back to the previous room
   @FXML
   private void onGoBack(ActionEvent event) throws ApiProxyException, IOException {
     GameState.currentObj = "RGB Puzzle";
@@ -347,6 +393,7 @@ public class PuzController {
     App.setUi("lockedroom");
   }
 
+  // Method to collect the rgb clue
   @FXML
   private void clickRgb() {
     GameState.isRgbClueFound = true;
